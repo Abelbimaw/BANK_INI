b311e8967c3f811bc2901939ff4b13064661a989
Abel Bima Wiratama <learn.smktelkom-mlg.sch.id>
2017-02-11 21:43:11 +0700

    ganti mode tampil formnya

--- src/bank/ini/BankService.java
+++ src/bank/ini/BankService.java
@@ -82,7 +82,8 @@ public class BankService extends javax.swing.JFrame {
             .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
-        pack();
+        setSize(new java.awt.Dimension(333, 257));
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void CBServiceActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CBServiceActionPerformed
